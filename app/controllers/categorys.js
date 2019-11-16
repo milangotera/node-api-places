ccbdd171674fed40e7a726636ec2d4d68b5b9863
Milan Gotera <milangotera@gmail.com>
2019-11-15 21:38:02 -0500

    AGregando categorias

--- app/controllers/categorys.js
+++ app/controllers/categorys.js
@@ -21,6 +21,50 @@ const CategorysController = {
         });
     },
 
+    create: function(req, res) {
+
+        let errors = 0;
+        let errorData = {};
+
+        if(!req.body.category_name){
+            errorData.category_name = "El campo nombre es requerido";
+            errors++;
+        }
+        if(!req.body.category_display){
+            errorData.category_display = "El campo display es requerido";
+            errors++;
+        }
+
+        if(errors){
+            return res.status(403).send({
+                status:403,
+                errors: errorData,
+                message: 'Error en envío de datos',
+            });
+        }
+
+        const categoryData = {
+            category_name: req.body.category_name,
+            category_display: req.body.category_display,
+            category_icon: 'category/0.png'
+        };
+
+        AppModel.insert('category', categoryData, function(error, result) {
+            if(error){
+                return res.status(500).send({
+                    status: 500,
+                    message: 'Error interno del servidor',
+                });
+            }
+            if(result){
+                return res.status(201).send({
+                    status: 201,
+                    message: 'Se ha registrado exitosamente',
+                });
+            }
+        });
+    },
+
     listPlaces: function(req, res) {
         let category_id = req.params.id;
         AppModel.select('view_places_list', { category_id : category_id }, function(error, result) {
--- app/routes/categorys.js
+++ app/routes/categorys.js
@@ -1,10 +1,15 @@
 'use strict';
 
-var router = require('express').Router();
-var CategorysController = require('../controllers/categorys');
+const router = require('express').Router();
+const CategorysController = require('../controllers/categorys');
+const authMiddleware = require('../middlewares/auth');
 
 router.get('/', function(req, res) {
     CategorysController.list(req, res);
+});
+
+router.post('/', authMiddleware.getToken, authMiddleware.getTokenAdmin, function(req, res) {
+    CategorysController.create(req, res);
 });
 
 router.get('/:id/places', function(req, res) {
